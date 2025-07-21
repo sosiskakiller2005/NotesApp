30110f6f885134205055ec766a9191bdd850c086
Влад Козлов <vkozlov15012005@gmail.com>
2025-07-21 13:46:54 +0300

    Добавлена проверка на пустой ввод в поле названия заметки. В случае пустого ввода инпут меняет цвет и показывает текст ошибки.

--- client/src/components/ui/CreateNoteForm.tsx
+++ client/src/components/ui/CreateNoteForm.tsx
@@ -1,37 +1,52 @@
 import type NoteProps from '@/props/NoteProps';
-import { Button, Field, Fieldset, Input, Stack, Textarea,  } from '@chakra-ui/react'
+import { Button, Field, Fieldset, Input, Stack, Textarea, } from '@chakra-ui/react'
 import { useState } from 'react';
 import '../../index.css'
 
 
 export default function CreateNoteForm({ onCreate }: { onCreate: (note: NoteProps) => void }) {
-  const [note, setNote] = useState({ title: '', description: '' });
-
-  const onSubmit = (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
-    const newNote: NoteProps = {
-      id: Date.now(),
-      title: note.title || '',
-      description: note.description || '',
-      createdAt: new Date(),
-      onDelete: () => {}, // заглушка, не делает ничего
-    };
-    setNote({ title: '', description: '' }); // Сброс формы после создания заметки
-    onCreate(newNote);
-    
+  
+  const [isTitleValid, setIsTitleValid] = useState(true);
+  const checkFields = (title: string) => {
+    const isValid = title === '' ? false :  true;
+    return isValid;
   }
 
-    return (
-        <Fieldset.Root className='w-full flex flex-col ' onSubmit={onSubmit}>
-          <Stack>
-            <Fieldset.Legend>Создание заметки</Fieldset.Legend>
-            <Field.Root>
-              <Input type="text" placeholder='Название заметки' value={note?.title ?? ''} onChange={(e) => setNote({...note, title: e.target.value})}/>
-              <Textarea name="" id="" placeholder='Описание' value={note?.description ?? ''} onChange={(e) => setNote({...note, description: e.target.value})}/>
-              <Button className='w-full' type='submit' onClick={onSubmit}>Создать</Button>
+  const [note, setNote] = useState({ title: '', description: '' });
+  const onSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    const isValid = checkFields(note.title);
+    setIsTitleValid(isValid);
+    if (isValid){
+      const newNote: NoteProps = {
+        id: Date.now(),
+        title: note.title,
+        description: note.description || '',
+        createdAt: new Date(),
+        onDelete: () => { }, // заглушка, не делает ничего
+      };
+      setNote({ title: '', description: '' }); // Сброс формы после создания заметки
+      onCreate(newNote);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Создание заметки</h1>
+      <Fieldset.Root className='w-full flex flex-col ' onSubmit={onSubmit}>
+        <Stack>
+          <Fieldset.Legend> </Fieldset.Legend>
+          <Field.Root>
+            <Field.Root invalid={!isTitleValid} >
+              <Input type="text" placeholder='Введите название заметки' value={note?.title ?? ''} onChange={(e) => setNote({ ...note, title: e.target.value })}></Input>
+              <Field.ErrorText>Название обязательно</Field.ErrorText>
             </Field.Root>
 
-          </Stack>
-        </Fieldset.Root>
-    );
+            <Textarea className='!mt-2' placeholder='Описание' value={note?.description ?? ''} onChange={(e) => setNote({ ...note, description: e.target.value })} />
+            <Button className='w-full' type='submit' onClick={onSubmit}>Создать</Button>
+          </Field.Root>
+        </Stack>
+      </Fieldset.Root>
+    </div>
+  );
 }
